61a95cb42cd2b7a8d5632240e2f71e728dea1800
hellrich <hellrich@7ac257a9-6a28-0410-b245-f828558649ed>
2012-12-12 14:35:03 +0000

    Fixed path in script
    git-svn-id: https://www.coling.uni-jena.de/svn/jules/trunk/jules-token-ae@7277 7ac257a9-6a28-0410-b245-f828558649ed

--- scripts/setCP.sh
+++ scripts/setCP.sh
@@ -1,11 +1,11 @@
 
 cd ../target
 CLASSPATH=`for i in *.jar; do echo -n "../target/$i:";done;`
-cd lib
-CLASSPATH="$CLASSPATH"`for i in *.jar; do echo -n "../target/dependency/$i:";done;`
+cd ../lib
+CLASSPATH="$CLASSPATH"`for i in *.jar; do echo -n "../lib/$i:";done;`
 CLASSPATH="$CLASSPATH."
 
 export CLASSPATH
 
-cd ../../scripts
+cd ../scripts
 
